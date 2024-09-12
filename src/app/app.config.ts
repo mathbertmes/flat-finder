bf787f4c0a5e53094a27e766642b82b1519e653b
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-11 19:16:57 -0700

    fix: firebase keys security

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,12 +12,12 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'flat-finder-b4591',
-        appId: '1:624544131282:web:efd0a5735b0842d8f9a50b',
-        storageBucket: 'flat-finder-b4591.appspot.com',
-        apiKey: 'AIzaSyDNaTqWKmyL6RHWYPgRO8sfb2dQJ6SKRe0',
-        authDomain: 'flat-finder-b4591.firebaseapp.com',
-        messagingSenderId: '624544131282',
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        appId: process.env['FIREBASE_APP_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        apiKey: process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
       })
     ),
     provideAuth(() => getAuth()),
