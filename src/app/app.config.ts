5721b137a9bab03561b15e040173c3e262637cdc
YUTAKURE <shiro.white.ffffff@gmail.com>
2024-09-11 19:27:27 -0700

    angular materials

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +22,6 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    provideFirestore(() => getFirestore()), provideAnimationsAsync(),
   ],
 };
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,104 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  form = new FormGroup(
+    {
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      birthDate: new FormControl('', [
+        Validators.required,
+        this.ageValidator(18),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    [this.matching('password', 'confirmPassword')]
+  );
 
+  get firstName() {
+    return this.form.get('firstName');
+  }
+
+  get lastName() {
+    return this.form.get('lastName');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+  get birthDate() {
+    return this.form.get('birthDate');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+  get confirmPassword() {
+    return this.form.get('confirmPassword');
+  }
+
+  matching(controlName: string, checkControlName: string): ValidatorFn {
+    return (controls: AbstractControl) => {
+      const control = controls.get(controlName);
+      const checkControl = controls.get(checkControlName);
+      if (control === null) return null;
+      if (checkControl === null) return null;
+      if (control.value !== checkControl.value) {
+        checkControl.setErrors({ matching: true });
+        return { matching: true };
+      }
+      return null;
+    };
+  }
+
+  ageValidator(minAge: number): ValidatorFn {
+    return (control: AbstractControl) => {
+      const birthDateValue = control.value;
+      if (!birthDateValue) return null;
+
+      const today = new Date();
+      const birthDateObj = new Date(birthDateValue);
+
+      let age = today.getFullYear() - birthDateObj.getFullYear();
+      const monthDifference = today.getMonth() - birthDateObj.getMonth();
+
+      if (
+        monthDifference < 0 ||
+        (monthDifference === 0 && today.getDate() < birthDateObj.getDate())
+      ) {
+        age--;
+      }
+
+      return age >= minAge ? null : { minAge: true };
+    };
+  }
+
+  onSubmit(): void {}
 }
