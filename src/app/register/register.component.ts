db4b0533f98bfc47cb1a47e881485445af5f7d44
YUTAKURE <shiro.white.ffffff@gmail.com>
2024-09-12 19:14:37 -0700

    fix: login and register css

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import {
   AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
   ValidatorFn,
   Validators,
@@ -12,19 +14,33 @@ import { NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { merge } from 'rxjs';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  http = inject(HttpClient)
-  router = inject(Router)
+  http = inject(HttpClient);
+  router = inject(Router);
   authService = inject(AuthService);
-  
+
   form = new FormGroup(
     {
       firstName: new FormControl('', [
@@ -37,7 +53,8 @@ export class RegisterComponent {
       ]),
       email: new FormControl('', [Validators.required, Validators.email]),
       birthDate: new FormControl('', [
-        Validators.required
+        Validators.required,
+        this.ageRangeValidator(18, 120),
       ]),
       password: new FormControl('', [
         Validators.required,
@@ -84,35 +101,133 @@ export class RegisterComponent {
     };
   }
 
-  ageValidator(minAge: number): ValidatorFn {
+  ageRangeValidator(minAge: number, maxAge: number): ValidatorFn {
     return (control: AbstractControl) => {
-      const birthDateValue = control.value;
-      if (!birthDateValue) return null;
+      if (!control.value) {
+        return null;
+      }
 
+      const birthDate = new Date(control.value);
       const today = new Date();
-      const birthDateObj = new Date(birthDateValue);
 
-      let age = today.getFullYear() - birthDateObj.getFullYear();
-      const monthDifference = today.getMonth() - birthDateObj.getMonth();
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDifference = today.getMonth() - birthDate.getMonth();
 
       if (
         monthDifference < 0 ||
-        (monthDifference === 0 && today.getDate() < birthDateObj.getDate())
+        (monthDifference === 0 && today.getDate() < birthDate.getDate())
       ) {
         age--;
       }
 
-      return age >= minAge ? null : { minAge: true };
+      if (age < minAge || age > maxAge) {
+        return { ageRange: true };
+      }
+
+      return null;
     };
   }
 
   onSubmit(): void {
-    const rawForm = this.form.getRawValue()
-    if(rawForm.email && rawForm.password && rawForm.firstName, rawForm.lastName){
-      this.authService.register(rawForm.email!, rawForm.password!, rawForm.firstName!, rawForm.lastName!)
-      .subscribe(() => {
-        this.router.navigateByUrl("/")
-      })
+    const rawForm = this.form.getRawValue();
+    if (
+      (rawForm.email && rawForm.password && rawForm.firstName, rawForm.lastName)
+    ) {
+      this.authService
+        .register(
+          rawForm.email!,
+          rawForm.password!,
+          rawForm.firstName!,
+          rawForm.lastName!
+        )
+        .subscribe(() => {
+          this.router.navigateByUrl('/');
+        });
     }
+  }
+
+  firstNameErrorMessage = signal('');
+  lastNameErrorMessage = signal('');
+  emailErrorMessage = signal('');
+  birthDateErrorMessage = signal('');
+  passwordErrorMessage = signal('');
+  confirmPasswordErrorMessage = signal('');
+
+  constructor() {
+    merge(this.email!.statusChanges, this.email!.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateEmailErrorMessage());
+  }
+
+  updateFirstNameErrorMessage() {
+    if (this.firstName!.hasError('required')) {
+      this.firstNameErrorMessage.set('You must enter a value');
+    } else if (this.firstName!.hasError('minlength')) {
+      this.firstNameErrorMessage.set(
+        'First must be at least 2 characters long.'
+      );
+    } else {
+      this.firstNameErrorMessage.set('');
+    }
+  }
+
+  updateLastNameErrorMessage() {
+    if (this.lastName!.hasError('required')) {
+      this.lastNameErrorMessage.set('You must enter a value');
+    } else if (this.lastName!.hasError('minlength')) {
+      this.lastNameErrorMessage.set('Last must be at least 2 characters long.');
+    } else {
+      this.firstNameErrorMessage.set('');
+    }
+  }
+
+  updateEmailErrorMessage() {
+    if (this.email!.hasError('required')) {
+      this.emailErrorMessage.set('You must enter a value');
+    } else if (this.email!.hasError('email')) {
+      this.emailErrorMessage.set('Not a valid email');
+    } else {
+      this.emailErrorMessage.set('');
+    }
+  }
+
+  updateBirthDateErrorMessage() {
+    if (this.birthDate!.hasError('required')) {
+      this.birthDateErrorMessage.set('You must enter a value');
+    } else if (this.birthDate!.hasError('ageRange')) {
+      this.birthDateErrorMessage.set('Age must be in the range of 18-120.');
+    } else {
+      this.birthDateErrorMessage.set('');
+    }
+  }
+
+  updatePasswordErrorMessage() {
+    if (this.password!.hasError('required')) {
+      this.passwordErrorMessage.set('You must enter a value');
+    } else if (this.password!.hasError('minlength')) {
+      this.passwordErrorMessage.set(
+        'Password must be at least 6 characters long.'
+      );
+    } else {
+      this.passwordErrorMessage.set('');
+    }
+  }
+
+  updateConfirmPasswordErrorMessage() {
+    if (this.confirmPassword!.hasError('required')) {
+      this.confirmPasswordErrorMessage.set('You must enter a value');
+    } else if (this.confirmPassword!.hasError('matching')) {
+      this.confirmPasswordErrorMessage.set(
+        'Password and confirm password must be the same.'
+      );
+    } else {
+      this.confirmPasswordErrorMessage.set('');
+    }
+  }
+
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
   }
 }
