dddcd21f4c476702ef02206a9cd89c2c12b44b4f
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-18 18:56:39 -0700

    fix: get flats function refactor

--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from "@angular/core";
 import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, query, updateDoc, where } from "@angular/fire/firestore";
 import { Flat } from "./interfaces/flat.interface";
+import { Observable } from "rxjs";
 
 
 
@@ -10,10 +11,19 @@ import { Flat } from "./interfaces/flat.interface";
 export class FirestoreService {
   firestore = inject(Firestore)
 
-
+  //CREATE
   async createFlat(flat: Flat): Promise<void> {
     const tasksCollection = collection(this.firestore, 'flats'); 
     await addDoc(tasksCollection, flat); 
+  }
+
+  //READ ALL FLATS
+  getFlats(): Observable<Flat[]> {
+    const tasksCollection = collection(this.firestore, 'flats'); 
+    const userTasksQuery = query(
+      tasksCollection
+    )
+    return collectionData(userTasksQuery, { idField: 'id' }) as Observable<Flat[]>;
   }
 
 }
