82fe73233304e5e3f758c3517623a770734bdcdf
YUTAKURE <shiro.white.ffffff@gmail.com>
2024-09-18 20:57:44 -0700

    feat: profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { NewFlatComponent } from './new-flat/new-flat.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'new-flat', component: NewFlatComponent },
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
 ];
