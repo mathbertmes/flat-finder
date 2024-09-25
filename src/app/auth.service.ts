3c451f2cfd17f4318be2c080cd0555e4d600dfbd
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-24 18:47:50 -0700

    feat: creating users in firestore and reading it

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,12 +8,15 @@ import {
   user,
 } from '@angular/fire/auth';
 import { from, Observable } from 'rxjs';
+import { FirestoreService } from './firestore.service';
+import { User } from './interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   firebaseAuth = inject(Auth);
+  firestoreFunctions = inject(FirestoreService);
   user$ = user(this.firebaseAuth);
   userId$: string | undefined;
 
@@ -23,13 +26,23 @@ export class AuthService {
     password: string,
     firstName: string,
     lastName: string,
+    birthDate: Date
   ): Observable<void> {
     const promise = createUserWithEmailAndPassword(
       this.firebaseAuth,
       email,
       password
     ).then((response) => {
+      let newUser : User = {
+        id: response.user.uid,
+        email,
+        firstName,
+        lastName,
+        birthDate
+      }
       updateProfile(response.user, { displayName: `${firstName} ${lastName}` });
+      this.firestoreFunctions.createUser(newUser)
+      localStorage.setItem("user", JSON.stringify(newUser))
       localStorage.setItem("userFullName", `${firstName} ${lastName}`);
       localStorage.setItem("userEmail", email)
       localStorage.setItem('userId', response.user.uid);
@@ -44,6 +57,9 @@ export class AuthService {
       email,
       password
     ).then((response) => {
+      this.firestoreFunctions.getUser(response.user.uid).subscribe((user) => {
+        localStorage.setItem("user", JSON.stringify(user))
+      })
       localStorage.setItem("userFullName", response.user.displayName!)
       localStorage.setItem("userEmail", response.user.email!)
       localStorage.setItem('userId', response.user.uid)
@@ -56,6 +72,8 @@ export class AuthService {
     const promise = signOut(this.firebaseAuth);
     localStorage.removeItem("userFullName")
     localStorage.removeItem('userId')
+    localStorage.removeItem('userEmail')
+    localStorage.removeItem('user')
     return from(promise);
   }
 }
--- src/app/firestore.service.ts
+++ src/app/firestore.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, query, updateDoc, where } from "@angular/fire/firestore";
 import { Flat } from "./interfaces/flat.interface";
 import { Observable } from "rxjs";
+import { User } from "./interfaces/user.interface";
 
 
 
@@ -17,11 +18,28 @@ export class FirestoreService {
     await addDoc(tasksCollection, flat); 
   }
 
+  async createUser(user: User): Promise<void> {
+    const usersCollection = collection(this.firestore, 'users'); 
+    await addDoc(usersCollection, user); 
+  }
+
+  getUser(id: string): Observable<User> {
+    const usersCollection = collection(this.firestore, 'users'); 
+    const userQuery = query(
+      usersCollection,
+      where("id", "==", id)
+    )
+
+    return collectionData(userQuery, { idField: 'id' }) as Observable<User>;
+  }
+
+
+
   //READ ALL FLATS
   getFlats(): Observable<Flat[]> {
-    const tasksCollection = collection(this.firestore, 'flats'); 
+    const flatsCollection = collection(this.firestore, 'flats'); 
     const userTasksQuery = query(
-      tasksCollection
+      flatsCollection
     )
     return collectionData(userTasksQuery, { idField: 'id' }) as Observable<Flat[]>;
   }
