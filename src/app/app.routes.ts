6c5e2739681856979ebaa6ec5caea2dd64cfaa37
Matheus Bertemes <mathbertemes@gmail.com>
2024-09-29 20:39:13 -0700

    flat view route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,14 @@ import { MyFlatsComponent } from './my-flats/my-flats.component';
 import { ProfileUpdateComponent } from './profile-update/profile-update.component';
 import { DeleteAccountComponent } from './delete-account/delete-account.component';
 import { AllUsersComponent } from './all-users/all-users.component';
+import { FlatViewComponent } from './flat-view/flat-view.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile-update', component: ProfileUpdateComponent },
   { path: 'profile/:id', component: ProfileComponent },
+  { path: 'flat-view/:id', component: FlatViewComponent },
   { path: 'new-flat', component: NewFlatComponent },
   { path: 'favorites', component: FavoritesComponent },
   { path: 'my-flats', component: MyFlatsComponent },
